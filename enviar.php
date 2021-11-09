5bff100030f595ec3930542678a6a9fa7f59e368
joaoHass <smartpocketbr@gmail.com>
2021-11-09 17:14:57 -0300

    refactored enviar code

--- enviar.php
+++ enviar.php
@@ -1,31 +1,42 @@
 <?php
 
-// Passando os dados obtidos pelo formulário para as variáveis abaixo
-$nomeremetente             = $_POST['name-form'];
-$emailremetente            = trim($_POST['email-form']);
-$telefone      	           = $_POST['cellphone-form'];
-$emaildestinatario         = 'leads@blancmarketingdigital.com.br,contato@essencialled.com.br'; // Digite seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
- 
- 
-/* Montando a motorizado a ser enviada no corpo do e-mail. */
+// Passando os dados obtidos pelo formulário para as variáveis abaixo e sanitizando
+$nomeremetente    = htmlspecialchars($_POST['name-form'], ENT_QUOTES);
+$emailremetente   = 'no-reply@blancmarketingdigital.com.br';
+$telefone      	  = htmlspecialchars($_POST['cellphone-form'], ENT_QUOTES);
+$url              = htmlspecialchars($_POST['url-form'], ENT_QUOTES);
+$emailatt_cliente = 'contato@essencialled.com.br';
+$emailatt_leads   = 'leads@blancmarketingdigital.com.br';
+
+/* =-=-=-= Informações base do e-mail (enviada para todos os destinatários) =-=-=-=*/
 $corpoHTML = '<strong>Formulário de Contato</strong>
 
-<p><b>Nome*:</b>                     '.$nomeremetente.'  </p>
-<p><b>E-Mail*:</b>                   '.$emailremetente.' </p>
-<p><b>Telefone*:</b>                 '.$telefone.'       </p>
+<p><b>Nome:</b>                     '.$nomeremetente.'  </p>
+<p><b>E-Mail:</b>                   '.$emailremetente.' </p>
+<p><b>Telefone:</b>                 '.$telefone.'       </p>
 <hr>';
-
 
 // O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
 // O return-path deve ser ser o mesmo e-mail do remetente.
 $headers  = "MIME-Version: 1.1\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
 $headers .= "From: $emailremetente\r\n"; // remetente
-$headers .= "Return-Path: $emaildestinatario \r\n"; // return-path
-$envio    =  mail($emaildestinatario,"Lead Essencial LED", $corpoHTML, $headers); 
+/* =-=-=-= Fim das informações base do e-mail =-=-=-= */
 
- if($envio) {
-   echo "<script>location.href='sucesso.html'</script>"; // Página que será redirecionada
- }
+
+// Envio para o e-mail do cliente
+$header_cliente = $headers . "Return-Path: $emailatt_cliente \r\n";
+$envio_cliente  = mail($emailatt_cliente,"[Lead] Essencial LED", $corpoHTML, $header_cliente);
+
+// Envio para o e-mail de leads
+$corpoHTML_leads = $corpoHTML . '<p><b> URL do Lead: </b>' .$url.' </p> <hr>';
+
+$header_leads    = $headers . "Return-Path: $emailatt_leads \r\n";
+$envio_leads     = mail($emailatt_leads,"[Lead] Essencial LED", $corpoHTML_leads, $header_leads); 
+
+
+// if($envio_cliente && $envio_leads) {
+//   echo "<script>location.href='sucesso.html'</script>"; // Página que será redirecionada
+// }
 
 ?>
